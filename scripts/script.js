308a41e9a4b27bc07ba1f7d9b4c44d6df82d66dc
elizaWentz <eliza.wentzel@gmail.com>
2025-09-15 16:04:46 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,16 @@
 // JavaScript Document
+
+
 console.log("hi");
+
+
+const filterButtons = document.querySelectorAll('.filter-button');
+
+filterButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    filterButtons.forEach(button => button.classList.remove('active'));
+    button.classList.add('active');
+  });
+});
+
+
